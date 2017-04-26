386f2603851179982a6095fd45087e2f77cec8a6
Jianyu Zuo <amosqqmail1@qq.com>
2017-04-26 16:27:09 -0500

    Forms Inputs Selects Buttons in HTML5

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('Hello world');
+console.log('Starting App...');
+// require <=> include/ import
+// import 'filesystem'
+const fs = require('fs');
+const os = require('os')
+
+var user = os.userInfo();
+/*
+fs.appendFile('File1.txt', 'Data buffer contents', function (err) {
+    if (err) {
+        console.log('File writting failed.')
+    }
+});
+*/
+console.log(user)
+//  ${user.username};
+
+
